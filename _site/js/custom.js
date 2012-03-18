731574caf04aa642ae60bd01fa32442eb101f080
Corey Ballou <ballouc@gmail.com>
2012-03-18 11:21:02 -0400

    Adding site dir to master.

---
+++ _site/js/custom.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function() {
+	
+	$('#gf').text('GitHub Followers');
+    $('#gfr').text('GitHub Repos');		
+	
+	JSONP( 'https://api.github.com/users/cballou?callback=?', function( response ) {
+		var data = response.data;
+		$('#gf').html('<strong>' + parseInt(data.followers) + '</strong> GitHub Followers');
+        $('#gfr').html('<strong>' + parseInt(data.public_repos) + '</strong> GitHub Repos');
+	});
+	
+	function JSONP( url, callback ) {
+		var id = ( 'jsonp' + Math.random() * new Date() ).replace('.', '');
+		var script = document.createElement('script');
+		script.src = url.replace( 'callback=?', 'callback=' + id );
+		document.body.appendChild( script );
+		window[ id ] = function( data ) {
+			if (callback) {
+				callback( data );
+			}
+		};
+	}	
+	
+	$('#ghw').githubWidget({
+		'username': 'cballou',
+		'displayActions': false,
+		'firstCount': 10,
+		'displayHeader': false,
+		'displayLastCommit': false,
+		'displayAccountInformations': false,
+		'displayLanguage': false
+	});
+});
