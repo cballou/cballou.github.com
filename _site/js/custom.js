1128059a05ebd876703b7399a633097ef4be93ef
Corey Ballou <ballouc@gmail.com>
2012-03-18 13:41:56 -0400

    Fixing up the main landing page. Added latest tweets.

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -30,4 +30,34 @@ jQuery(document).ready(function() {
 		'displayAccountInformations': false,
 		'displayLanguage': false
 	});
+	
+	displayTweets();
 });
+
+function displayTweets() {
+	var twitter_api_url = 'http://search.twitter.com/search.json';
+	var twitter_user    = 'cballou';
+ 
+	// Enable caching
+	$.ajaxSetup({ cache: true });
+	$.getJSON(
+		twitter_api_url + '?callback=?&rpp=3&q=from:' + twitter_user,
+		function(data) {
+			$.each(data.results, function(i, tweet) {
+				if (tweet.text !== undefined) {
+					var date_tweet = new Date(tweet.created_at);
+					var date_now   = new Date();
+					var date_diff  = date_now - date_tweet;
+					var hours      = Math.round(date_diff/(1000*60*60));
+					var tweet_html = '<li class="tweet_text">';
+					tweet_html    += '<a href="http://www.twitter.com/';
+					tweet_html    += twitter_user + '/status/' + tweet.id + '">';
+					tweet_html    += tweet.text + '<\/a>';
+					tweet_html    += '<div class="tweet_hours">' + hours;
+					tweet_html    += ' hours ago<\/div><\/li>';
+					$('#tweet_container').append(tweet_html);
+				}
+			});
+		}
+	);
+}
